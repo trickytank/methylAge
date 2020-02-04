c8004025c5244eca4909374c8a0e01567501b238
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-02-04 15:06:00 +0800

    data check test

---
+++ tests/testthat/test-check_methylation_data.R
@@ -0,0 +1,22 @@
+# Smaller dataset to test with :p
+metm <- met[1:3, 1:4]
+
+test_that("No warnings or errors on clean data", {
+  expect_silent(check_methylation_data(metm))
+})
+
+test_that("Warning when more rows than columns", {
+  expect_warning(check_methylation_data(metm[1:3, 1:2]))
+})
+
+test_that("Error with wrong data type", {
+  expect_error(check_methylation_data(1))
+})
+
+metm_col <- metm
+metm_row <- metm
+colnames(metm_col) <- rownames(metm_row) <- NULL
+test_that("Error when no column or row names", {
+  expect_error(check_methylation_data(metm_col))
+  expect_error(check_methylation_data(metm_row))
+})
