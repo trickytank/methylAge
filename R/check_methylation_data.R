55186cd826da829d6a8d352abf1fa2f039e619b6
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-02-04 15:06:15 +0800

    make sure we use the actual data :o

--- R/check_methylation_data.R
+++ R/check_methylation_data.R
@@ -1,5 +1,5 @@
 check_methylation_data <- function(x, dim_warning = TRUE) {
-  assert_true(is.matrix(starwars) || is.data.frame(starwars))
+  assert_true(is.matrix(x) || is.data.frame(x))
   assert_false(is.null(rownames(x)))
   assert_false(is.null(colnames(x)))
   if(dim_warning && nrow(x) > ncol(x)) {
