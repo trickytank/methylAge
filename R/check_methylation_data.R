22920d0ad7c33e92cf20ea5cacdd5f3706f3fea6
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-02-04 16:11:01 +0800

    check methylation data doc

--- R/check_methylation_data.R
+++ R/check_methylation_data.R
@@ -1,5 +1,9 @@
+#' Check methylation data is formatted in a way suitable for the methylAge package
+#' @export
+#' @import checkmate
 check_methylation_data <- function(x, dim_warning = TRUE) {
   assert_true(is.matrix(x) || is.data.frame(x))
+  assert_false(is_tibble(x))
   assert_false(is.null(rownames(x)))
   assert_false(is.null(colnames(x)))
   if(dim_warning && nrow(x) > ncol(x)) {
