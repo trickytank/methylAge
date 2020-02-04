e3d5d7aaf93e1484972f2b4a37fe4b5d826c4fbd
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-02-04 14:47:52 +0800

    methylation data check

---
+++ R/check_methylation_data.R
@@ -0,0 +1,8 @@
+check_methylation_data <- function(x, dim_warning = TRUE) {
+  assert_true(is.matrix(starwars) || is.data.frame(starwars))
+  assert_false(is.null(rownames(x)))
+  assert_false(is.null(colnames(x)))
+  if(dim_warning && nrow(x) > ncol(x)) {
+    warning("Number of samples (rows) is more than the number of probes (rows)", immediate. = TRUE)
+  }
+}
