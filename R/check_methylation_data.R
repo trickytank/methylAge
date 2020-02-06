906aa8d5187b0d2f4b7d79063ddcc2855447596b
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-02-06 15:34:26 +0800

    explicit package of tibble

--- R/check_methylation_data.R
+++ R/check_methylation_data.R
@@ -3,7 +3,7 @@
 #' @import checkmate
 check_methylation_data <- function(x, dim_warning = TRUE) {
   assert_true(is.matrix(x) || is.data.frame(x))
-  assert_false(is_tibble(x))
+  assert_false(tibble::is_tibble(x))
   assert_false(is.null(rownames(x)))
   assert_false(is.null(colnames(x)))
   if(dim_warning && nrow(x) < ncol(x)) {
