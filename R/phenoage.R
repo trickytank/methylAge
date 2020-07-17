b5b9ee2e0f73bd5c32d83686420ab1953d94515b
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-07-17 19:49:55 +0800

    use default mark and coefficient col in phenoage

--- R/phenoage.R
+++ R/phenoage.R
@@ -7,7 +7,6 @@ phenoage <- function(x, id_col = "ID", age_col = "phenoage", allow_missing = FAL
   # Calculate Hannum Methylation Age
   generic_clock(
                 x, coef = phenoage_coef,
-                marker_col = "CpG", coef_col = "Weight",
                 id_col = id_col, age_col = age_col,
                 allow_missing = allow_missing, dim_warning = dim_warning,
                 clock_name = "Phenoage"
