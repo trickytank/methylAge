56fa14ac2a119db0752aa517e898b690a5705559
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-07-17 19:49:25 +0800

    import Zhang example age

--- data-raw/zhang_example.R
+++ data-raw/zhang_example.R
@@ -14,8 +14,9 @@ library(tools)
 library(fs)
 
 local_zhang_methylation <- 'data-raw/supplementary/zhang/data.rds'
+local_zhang_age <- 'data-raw/supplementary/zhang/data.age'
 
-
+  data.age
 #
 dir_create(path_dir(local_zhang_methylation))
 
@@ -26,6 +27,16 @@ if(md5sum(local_zhang_methylation) != "b5af935a6fbb89a6dc632243e45c69da") {
   stop("Zhang methylation example data file does not appear to be correct according to the md5sum.")
 }
 
+if(!file.exists(local_zhang_age)) {
+  download.file('https://raw.githubusercontent.com/qzhang314/DNAm-based-age-predictor/13b213b9644d6661556cf30129f1c17a22c57274/data.age', local_zhang_age)
+}
+if(md5sum(local_zhang_age) != "81965c06e7b85e8c8f98928244e34b5e") {
+  stop("Zhang methylation example age file does not appear to be correct according to the md5sum.")
+}
+
 met <- t(readRDS(local_zhang_methylation))
 
+met_age <- readr::read_delim(local_zhang_age, " ")
+
 use_data(met)
+use_data(met_age)
