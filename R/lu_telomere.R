573b19e7dd84ce2a01732fc98d96546b5316172d
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-07-17 16:09:45 +0800

    Lu Telomere length

---
+++ R/lu_telomere.R
@@ -0,0 +1,14 @@
+#' Lu DNA methylation telomere length (DNAmTL) calculation
+#' @export
+#' @import checkmate
+#' @import dplyr
+#' @import tibble
+lu_telomere <- function(x, id_col = "ID", age_col = "DNAmTL", allow_missing = FALSE, dim_warning = TRUE) {
+  # Calculate Hannum Methylation Age
+  generic_clock(
+                x, coef = lu_coef,
+                id_col = id_col, age_col = age_col,
+                allow_missing = allow_missing, dim_warning = dim_warning,
+                clock_name = "Lu DNAmTL"
+  )
+}
